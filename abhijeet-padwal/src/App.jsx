e51c0f4a5ed91df9b6f263368d93df6527cf142f
Abhijeet Padwal <38448205+padwalab@users.noreply.github.com>
2026-05-13 21:28:28 +0000

    Add simple React app for AbhijeetPadwal

---
+++ abhijeet-padwal/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <h1>AbhijeetPadwal</h1>
+      <p>Welcome to your React app</p>
+    </main>
+  )
+}
+
+export default App
